d7ac678b9b546918569127b4525db68395e6a4a1
Johannes Lamers <johanneslamers@gmail.com>
2017-02-28 23:11:03 +0100

    Update backup-files.sh

--- backup-files.sh
+++ backup-files.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
-CLIENT="XXX"
-BACKUP_NAME="XXX-SITE"
-
 # Warning: without leading '/' -> home/forge/default/public/static/
 BACKUP_DIR="home/forge/default/public/static/"
 
 WEEK=`date +%V`
 DATE=`date +%Y%m%d%H%M`
 
-SAVE_DIR="/home/forge/backup"
-S3_BUCKET="XXX-bucket"
-
 echo "backing up files..."
 
 # Backup site dir
-tar czf ${SAVE_DIR}/${BACKUP_NAME}-${DATE}.tar -C / ${BACKUP_DIR}
+tar czf ${SAVE_DIR}/${BACKUP_NAME}-files-${DATE}.tar -C / ${BACKUP_DIR}
 
-if [ -e ${SAVE_DIR}/${BACKUP_NAME}-${DATE}.tar ]; then
+if [ -e ${SAVE_DIR}/${BACKUP_NAME}-files-${DATE}.tar ]; then
 
     # Upload to AWS
-    aws s3 cp ${SAVE_DIR}/${BACKUP_NAME}-${DATE}.tar s3://${S3_BUCKET}/${CLIENT}/${WEEK}/files/${BACKUP_NAME}-${DATE}.tar
+    aws s3 cp ${SAVE_DIR}/${BACKUP_NAME}-files-${DATE}.tar s3://${S3_BUCKET}/${CLIENT}/${WEEK}/files/${BACKUP_NAME}-files-${DATE}.tar
 
     # Test result of last command run
     if [ "$?" -ne "0" ]; then
@@ -29,7 +23,7 @@ if [ -e ${SAVE_DIR}/${BACKUP_NAME}-${DATE}.tar ]; then
     fi
 
     # If success, remove backup file
-    rm ${SAVE_DIR}/${BACKUP_NAME}-${DATE}.tar
+    rm ${SAVE_DIR}/${BACKUP_NAME}-files-${DATE}.tar
 
     echo "Local backup removed"
     # Exit with no error
