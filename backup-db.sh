3e0554db6428c06d17f612fac5e78e7099e8b41b
Johannes Lamers <johanneslamers@gmail.com>
2017-02-28 22:21:30 +0100

    Create backup-db.sh

---
+++ backup-db.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+CLIENT="XXX"
+BACKUP_NAME="XXX-DB"
+
+WEEK=`date +%V`
+DATE=`date +%Y%m%d%H%M`
+
+SAVE_DIR="/home/forge/backup"
+S3_BUCKET="madebyjohannes-client-backup"
+
+# Get MYSQL_USER and MYSQL_PASSWORD
+source /home/forge/backup/.env
+
+mysqldump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} | gzip > ${SAVE_DIR}/${BACKUP_NAME}.sql.gz
+
+if [ -e ${SAVE_DIR}/${BACKUP_NAME}-${DATE}.sql.gz ]; then
+
+    # Upload to AWS
+    aws s3 cp ${SAVE_DIR}/${BACKUP_NAME}-${DATE}.sql.gz s3://${S3_BUCKET}/${CLIENT}/${WEEK}/${BACKUP_NAME}-${DATE}.sql.gz
+
+    # Test result of last command run
+    if [ "$?" -ne "0" ]; then
+        echo "Upload to AWS failed"
+        exit 1
+    fi
+
+    # If success, remove backup file
+    rm ${SAVE_DIR}/${BACKUP_NAME}-${DATE}.sql.gz
+
+    # Exit with no error
+    exit 0
+elif [ ! -w "${TEMPDIR}" ]; then
+
+
+fi
+
+# Exit with error if we reach this point
+echo "Backup file not created"
+exit 1
