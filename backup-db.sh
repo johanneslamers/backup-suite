c58ab0712b4e9415cb845c4d527be1f08b07ae1e
johanneslamers <johanneslamers@gmail.com>
2017-03-01 17:26:19 +0100

    some fixes

--- backup-db.sh
+++ backup-db.sh
@@ -29,29 +29,43 @@ echo "Backing up database..."
 # http://serverfault.com/questions/7503/how-to-determine-if-a-bash-variable-is-empty
 if [ -n "$MYSQL_DATABASE" ];
 then
+
     # Temporary db dump path
     BACKUP_DB_PATH=${SAVE_DIR}/${BACKUP_NAME}-${DATE}-${MYSQL_DATABASE}.sql
+
+    echo "Backing up single database: "${MYSQL_DATABASE}
+    echo " "
 
     # Setup ignored tables
     IGNORED_TABLES_STRING=''
     for TABLE in "${EXCLUDED_TABLES[@]}"
     do
-        IGNORED_TABLES_STRING+=" --ignore-table=${DB}.${TABLE}"
+        IGNORED_TABLES_STRING+=" --ignore-table=${MYSQL_DATABASE}.${TABLE}"
     done
-    
-    # repair and optimize database
-    mysqlcheck -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} --auto-repair --optimize
+
+    echo "Ignored tables: "${IGNORED_TABLES_STRING}
+    echo " "
 
     # dump database to its own sql file
     mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} --single-transaction --no-data > ${BACKUP_DB_PATH}
     mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} --no-create-info ${IGNORED_TABLES_STRING} >> ${BACKUP_DB_PATH}
 
+    # repair and optimize database
+    mysqlcheck -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} --auto-repair --optimize --silent
+    echo " "
+
 else
+
+    echo "Backing up all databases"
+    echo " "
 
     # repair, optimize, and dump each database to its own sql file
     for DB in $(mysql -h$MYSQL_HOST -P$MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD -BNe 'show databases' | grep -Ev 'mysql|information_schema|performance_schema')
     do
 
+
+        echo "Backing up database: "${DB}
+        echo " "
         # Temporary db dump path
         BACKUP_DB_PATH=${SAVE_DIR}/${BACKUP_NAME}-${DATE}-${DB}.sql
 
@@ -62,12 +76,13 @@ else
             IGNORED_TABLES_STRING+=" --ignore-table=${DB}.${TABLE}"
         done
 
-        # repair and optimize database
-        mysqlcheck -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${DB} --auto-repair --optimize
-
         # dump each database to its own sql file
         mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${DB} --single-transaction --no-data > ${BACKUP_DB_PATH}
         mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${DB} --no-create-info ${IGNORED_TABLES_STRING} >> ${BACKUP_DB_PATH}
+
+        # repair and optimize database
+        mysqlcheck -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${DB} --auto-repair --optimize --silent
+        echo " "
 
     done
 
