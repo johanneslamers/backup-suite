889b0809a7a73f39c4a55049d7064f83e54dd7a9
johanneslamers <johanneslamers@gmail.com>
2017-03-01 12:23:24 +0100

    Rewrite backup script for multiple directories

--- backup-files.sh
+++ backup-files.sh
@@ -6,32 +6,54 @@ DATE=`date +%Y%m%d%H%M`
 # Get all enviroment cridentials
 source /home/forge/backup/.env
 
-echo "Backing up files..."
-
-# Backup site dir
-tar czf ${SAVE_DIR}/${BACKUP_NAME}-files-${DATE}.tar -C / ${BACKUP_DIR}
-
-if [ -e ${SAVE_DIR}/${BACKUP_NAME}-files-${DATE}.tar ]; then
-
-    # Upload to AWS
-    aws s3 cp ${SAVE_DIR}/${BACKUP_NAME}-files-${DATE}.tar s3://${S3_BUCKET}/${CLIENT}/${WEEK}/files/${BACKUP_NAME}-files-${DATE}.tar
-
-    # Test result of last command run
-    if [ "$?" -ne "0" ]; then
-        echo "Upload to AWS failed"
-        exit 1
-    fi
-
-    # If success, remove backup file
-    rm ${SAVE_DIR}/${BACKUP_NAME}-files-${DATE}.tar
-
-    echo "Local backup removed"
-    # Exit with no error
-    exit 0
+## Check we can write to the backups directory
+if [ -w "${SAVE_DIR}" ]
+then
+    # Do nothing and move along.
+    echo "Found and is writable: "${SAVE_DIR}
+    echo " "
+else
+    echo "Can't write to: "${SAVE_DIR}
+    exit
 fi
 
+echo "Backing up files..."
+
+## Backup of all directories
+
+for i in "${DIRECTORIES[@]}"
+do
+    # Make backup file variable
+    BACKUP_FILE=${BACKUP_NAME}-${DATE}-$(echo $i | sed 's/\//-/g').tar.gz
+
+    echo "Backing up "$i" -> "${BACKUP_FILE}
+
+    # Create tarball and compress it
+    tar zcf ${SAVE_DIR}/${BACKUP_FILE} -C / $i 2>&1
+    echo " "
+
+    # Upload file to S3
+    if [ -e ${BACKUP_FILE} ]; 
+    then
+        aws s3 cp ${SAVE_DIR}/${BACKUP_FILE} s3://${S3_BUCKET}/${CLIENT}/${WEEK}/files/${BACKUP_FILE} 2>&1
+        echo " "
+
+        # Test result of last command run
+        if [ "$?" -ne "0" ]; 
+        then
+            echo "Upload to AWS S3 failed"
+            exit 1
+        else
+            echo "Upload to AWS S3 successful"
+            echo "Removing local backup file"
+            # If success, remove backup file
+            rm ${SAVE_DIR}/${BACKUP_FILE}
+            echo "Local backup file "${BACKUP_FILE}" removed"
+            echo " "
+        fi
+    fi
+done
 
 # Exit with error if we reach this point
-echo "Backup file not created"
+echo "All Done! Yay!"
 exit 1
-
